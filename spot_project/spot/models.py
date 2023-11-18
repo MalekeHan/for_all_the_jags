0d43d85967e75d0275df81009230274b7aadc668
Maleke Hanson <71529357+MalekeHan@users.noreply.github.com>
2023-11-18 15:04:53 -0800

    Tas 28 (#8)
    * updated new model and added tests
    
    * updated new model and added tests

--- spot_project/spot/models.py
+++ spot_project/spot/models.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 from django.db import models
 from django.db.models.aggregates import Avg
+from django.utils import timezone
+from django.db.models import F, FloatField, ExpressionWrapper
+from django.db.models.functions import Now, Extract
+from django.utils.timezone import make_aware
 
 # Create your models here.
 
@@ -43,73 +47,97 @@ class Survey(models.Model):
     
     # Defualt values are None becuase some survey responses may not have all the fields
     # Model Fields
-    busy_level = models.IntegerField(choices=BUSY_CHOICES, default=None)
-    busy_str = models.CharField(choices=BUSY_CHOICES, default=None)
+    # busy_level = models.IntegerField(choices=BUSY_CHOICES, default=None)
+    # busy_str = models.CharField(choices=BUSY_CHOICES, default=None)
 
-    comfort_level = models.IntegerField(choices=COMFORT_CHOICES, default=None)
-    comfort_str = models.CharField(choices=COMFORT_CHOICES, default=None)
+    # comfort_level = models.IntegerField(choices=COMFORT_CHOICES, default=None)
+    # comfort_str = models.CharField(choices=COMFORT_CHOICES, default=None)
 
-    wifi_situation = models.IntegerField(choices=WIFI_CHOICES, default=None)
-    wifi_str = models.CharField(choices=WIFI_CHOICES, default=None)
+    # wifi_situation = models.IntegerField(choices=WIFI_CHOICES, default=None)
+    # wifi_str = models.CharField(choices=WIFI_CHOICES, default=None)
     
-    noise_level = models.IntegerField(choices=NOISE_CHOICES, default=None)
-    noise_str = models.CharField(choices=NOISE_CHOICES, default=None)
+    # noise_level = models.IntegerField(choices=NOISE_CHOICES, default=None)
+    # noise_str = models.CharField(choices=NOISE_CHOICES, default=None)
 
-    parking_situation = models.IntegerField(choices=PARKING_CHOICES, default=None)
-    parking_str = models.CharField(choices=PARKING_CHOICES, default=None)
+    # parking_situation = models.IntegerField(choices=PARKING_CHOICES, default=None)
+    # parking_str = models.CharField(choices=PARKING_CHOICES, default=None)
+
+    busy_level = models.IntegerField(choices=BUSY_CHOICES, default=None, null=True, blank=True)
+    comfort_level = models.IntegerField(choices=COMFORT_CHOICES, default=None, null=True, blank=True)
+    wifi_situation = models.IntegerField(choices=WIFI_CHOICES, default=None, null=True, blank=True)
+    noise_level = models.IntegerField(choices=NOISE_CHOICES, default=None, null=True, blank=True)
+    parking_situation = models.IntegerField(choices=PARKING_CHOICES, default=None, null=True, blank=True)
 
     location = models.ForeignKey(Location, on_delete=models.CASCADE) # location of the spot
 
-    timestamp = models.DateTimeField(auto_now_add=True) # time of the survey
+    timestamp = models.DateTimeField(default=timezone.now) # time of the survey
+
+    def get_busy_str(self):
+        return dict(Survey.BUSY_CHOICES).get(self.busy_level)
+
+    def get_comfort_str(self):
+        return dict(Survey.COMFORT_CHOICES).get(self.comfort_level)
+
+    def get_wifi_str(self):
+        return dict(Survey.WIFI_CHOICES).get(self.wifi_situation)
+
+    def get_noise_str(self):
+        return dict(Survey.NOISE_CHOICES).get(self.noise_level)
+
+    def get_parking_str(self):
+        return dict(Survey.PARKING_CHOICES).get(self.parking_situation)
 
     def __str__(self):
         return f"Survey #{self.id}"
 
     @staticmethod
     def get_weighted_avg(location_id):
-        # Get all objects for the given location
+        # Get the location object based on the provided ID
         location = Location.objects.get(pk=location_id)
-        queryset = Survey.objects.filter(location=location)
 
-        # Calculate weights based on timestamp
-        now = datetime.now()
-        queryset = queryset.annotate(
-            time_diff=models.F('timestamp') - now,
-            weight=models.Func(
-                # Square root to introduce quadratic fall-off (i.e. more recent surveys are weighted more heavily)
-                models.F('time_diff'),
-                function='POW',
-                template='SQRT(EXTRACT(EPOCH FROM %s))',  
+        # Get the current time, considering the timezone
+        now = timezone.now()
+        total_weight = 0
 
-            ),
-        )
-
-        # Calculate the total weight (sum of all weights)
-        total_weight = sum(entry.weight for entry in queryset)
-
-        # Calculate weighted averages for each field
-        busy_avg = sum(entry.busy_level * (entry.weight / total_weight) for entry in queryset)
-        comfort_avg = sum(entry.comfort_level * (entry.weight / total_weight) for entry in queryset)
-        wifi_avg = sum(entry.wifi_situation * (entry.weight / total_weight) for entry in queryset)
-        noise_avg = sum(entry.noise_level * (entry.weight / total_weight) for entry in queryset)
-        parking_avg = sum(entry.parking_situation * (entry.weight / total_weight) for entry in queryset)
-
-        return {
-            'busy_level': busy_avg,
-            'comfort_level': comfort_avg,
-            'wifi_situation': wifi_avg,
-            'noise_level': noise_avg,
-            'parking_situation': parking_avg,
+        # Initialize dictionary for storing weighted values
+        weighted_values = {
+            'busy_level': 0,
+            'comfort_level': 0,
+            'wifi_situation': 0,
+            'noise_level': 0,
+            'parking_situation': 0
         }
+
+        # Iterate over each survey related to the specified location
+        for survey in Survey.objects.filter(location=location):
+            # Calculate the time difference in seconds and apply quadratic fall-off (square root)
+            time_diff = max((now - survey.timestamp).total_seconds(), 1)  # Ensure at least 1 second to avoid division by zero
+            weight = time_diff ** 0.5  # apply quadratic fall-off
+            total_weight += weight
+
+            # Aggregate the survey's weighted responses for each category
+            for key in weighted_values:
+                value = getattr(survey, key, None)
+                if value is not None:
+                    weighted_values[key] += value * weight
+
+        # Normalize the aggregated values by dividing by the total weight (if greater than zero)
+        if total_weight > 0:
+            for key in weighted_values:
+                weighted_values[key] /= total_weight
+
+        return weighted_values
+    
 
     @staticmethod
     def get_todays_avg(location_id):
-        # get today's objects
-        queryset = Survey.objects.filter(survey_date=datetime.date.today())
+        # Get today's date
+        today = timezone.now().date()
 
-        location = Location.objects.get(pk=location_id)
-        queryset = queryset.filter(location=location)
+        # Filter surveys for the given location and created today
+        queryset = Survey.objects.filter(location_id=location_id, timestamp__date=today)
 
+        # Aggregate and return averages
         return queryset.aggregate(
             Avg('busy_level'),
             Avg('comfort_level'),
--- spot_project/spot/tests.py
+++ spot_project/spot/tests.py
@@ -19,17 +19,24 @@ class ModelTests(TestCase):
     def test_create_survey(self):
         """Test creating a survey with valid data is successful"""
         survey = Survey.objects.create(
-            busy_level='NB',
-            comfort_level='VC',
-            wifi_situation='NW',
-            noise_level='QZ',
-            parking_situation='EP',
+            busy_level=2,  # using integers as per the new model definition
+            comfort_level=2,
+            wifi_situation=2,
+            noise_level=2,
+            parking_situation=2,
             location=self.location
         )
-        self.assertEqual(survey.busy_level, 'NB')
+        self.assertEqual(survey.busy_level, 2)
         self.assertEqual(survey.location, self.location)
+    
+    def test_get_busy_str(self):
+        """Test the get_busy_str method returns correct string"""
+        survey = Survey.objects.create(busy_level=0, location=self.location)
+        self.assertEqual(survey.get_busy_str(), 'Very busy')
 
 from django.db.models import Avg
+from datetime import datetime, timedelta
+from django.utils import timezone
 
 class SurveyAggregateViewTests(TestCase):
 
@@ -37,28 +44,55 @@ class SurveyAggregateViewTests(TestCase):
         self.client = APIClient()
         self.location = Location.objects.create(name='Test Location', address='123 Test St')
 
-        # create surveys
-        Survey.objects.create(busy_level='NB', comfort_level='VC', wifi_situation='NW', noise_level='QZ', parking_situation='EP', location=self.location)
-        Survey.objects.create(busy_level='MB', comfort_level='MC', wifi_situation='PW', noise_level='MN', parking_situation='MP', location=self.location)
-        self.surveys_url = reverse('survey-aggregate') #dynamically generate the URL for the view --Django will go and look up this name in our urls and find the url path "/aggregate"
+        # Create surveys with different timestamps
+        Survey.objects.create(
+            busy_level=2,
+            comfort_level=2,
+            wifi_situation=2,
+            noise_level=2,
+            parking_situation=2,
+            location=self.location,
+            timestamp=timezone.now()  # Survey created today
+        )
+
+        Survey.objects.create(
+            busy_level=1,
+            comfort_level=1,
+            wifi_situation=1,
+            noise_level=1,
+            parking_situation=1,
+            location=self.location,
+            timestamp=timezone.now() - timedelta(days=30)  # Survey created last month
+        )
+
+        self.surveys_url = reverse('survey-aggregate')  # Ensure this URL name matches your URL configuration
 
     def test_aggregate_data(self):
         """Test retrieving aggregated survey data"""
         response = self.client.get(self.surveys_url, {'location_id': self.location.id})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        # have to manually assign the numerical vals to calc averages here
-        surveys = Survey.objects.filter(location=self.location) 
-        total_busy_level = sum(1 if s.busy_level == 'NB' else 2 if s.busy_level == 'MB' else 3 for s in surveys)
-        average_busy_level = total_busy_level / surveys.count() if surveys.count() > 0 else 0
+        # Here, you should compare response.data with the expected aggregated data
+        # Since you're using a custom aggregation method, you may need to calculate the expected results manually or use the same method in the test
+        aggregated_data = Survey.get_weighted_avg(self.location.id)
+        self.assertAlmostEqual(response.data['busy_level'], aggregated_data['busy_level'])
 
-        total_comfort_level = sum(1 if s.comfort_level == 'VC' else 2 if s.comfort_level == 'MC' else 3 for s in surveys)
-        average_comfort_level = total_comfort_level / surveys.count() if surveys.count() > 0 else 0
+    def test_get_todays_avg(self):
+        # Call get_todays_avg and verify the results
+        todays_avg = Survey.get_todays_avg(self.location.id)
 
-        # check assertions
-        self.assertAlmostEqual(response.data['average_busy_level'], average_busy_level)
-        self.assertAlmostEqual(response.data['average_comfort_level'], average_comfort_level)
+        # Expected averages should be based on surveys created today only
+        expected_busy_avg = 2  # As there's only one survey today with busy_level = 2
+        expected_comfort_avg = 2
+        expected_wifi_avg = 2
+        expected_noise_avg = 2
+        expected_parking_avg = 2
 
+        self.assertAlmostEqual(todays_avg['busy_level__avg'], expected_busy_avg)
+        self.assertAlmostEqual(todays_avg['comfort_level__avg'], expected_comfort_avg)
+        self.assertAlmostEqual(todays_avg['wifi_situation__avg'], expected_wifi_avg)
+        self.assertAlmostEqual(todays_avg['noise_level__avg'], expected_noise_avg)
+        self.assertAlmostEqual(todays_avg['parking_situation__avg'], expected_parking_avg)
 
 
 
