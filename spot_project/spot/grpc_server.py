8fa9b8a125f4a5ca31fdafa58d513753b1df83cf
colmpat <cplang@dons.usfca.edu>
2023-11-18 21:58:30 -0800

    refactor: make location/query logic per session

--- spot_project/spot/grpc_server.py
+++ spot_project/spot/grpc_server.py
@@ -1,23 +1,30 @@
-from typing import Iterator
+from typing import Iterator 
 import grpc
 from concurrent import futures
 
-from spot.proto.spot_pb2 import LocationPan, LocationZoom, Parameter, QueryUpdate
+from spot.proto.spot_pb2 import Parameter, QueryUpdate
 from spot.proto.spot_pb2_grpc import LocationServiceServicer, add_LocationServiceServicer_to_server
 from spot.models import Survey
 from spot.models import Location
+from spot.yelp import YelpHandler
+from geopy.distance import geodesic
 
-class Cache:
+class SessionState:
+    lat = 0.0
+    lon = 0.0
+    radius = 0.0
+    categories = set()
+    attributes = set()
+
     def __init__(self, max_size=20):
         self.max_size = max_size
         self.locations = []
 
-    def add_location(self, location_id):
+    def add_location(self, location: Location):
         # Calculate the aggregated score for the location
-        aggregated_score = Survey.get_weighted_avg(location_id)
+        aggregated_score = Survey.get_weighted_avg(location.id)
         total_score = self.calculate_total_score(aggregated_score)
 
-        location = Location.objects.get(pk=location_id)
         location_with_score = {
             'location': location,
             'score': total_score
@@ -26,15 +33,17 @@ class Cache:
         # Add location to cache and maintain sorting based on score
         self.locations.append(location_with_score)
         self.locations.sort(key=lambda loc: loc['score'], reverse=True)
+        self.invalidate_cache()
         self._trim_cache()
+    
+    def _trim_cache(self):
+        if len(self.locations) > self.max_size:
+            self.locations = self.locations[:self.max_size]
 
     def calculate_total_score(self, aggregated_values):
         return sum(aggregated_values.values())
 
     def remove_locations_outside_radius(self, center_lat, center_lon, radius):
-
-        # we still need to write to the DB here
-
         self.locations = [
             loc for loc in self.locations 
             if self.is_within_radius(loc['location'], center_lat, center_lon, radius)
@@ -43,58 +52,62 @@ class Cache:
     # distance between the location and the center point
     @staticmethod
     def is_within_radius(location, center_lat, center_lon, radius):
-        return Cache.calculate_distance(location.lat, location.lon, center_lat, center_lon) <= radius
+        return SessionState.calculate_distance(location.lat, location.lon, center_lat, center_lon) <= radius
 
+    @staticmethod
     def calculate_distance(lat1, lon1, lat2, lon2):
-        pass # i dont know if this is needed tbh
+        distance = geodesic((lat1, lon1), (lat2, lon2)).meters
+        return distance
+
+    def invalidate_cache(self):
+        # Check for each location in the cache if it is within the current radius
+        if self.radius > 0:
+            self.remove_locations_outside_radius(self.lat, self.lon, self.radius)
 
 
 class LocationServicer(LocationServiceServicer):
-    lat = 0
-    lon = 0
-    radius = 0
-    categories = {}
-    attributes = {}
-
-
-    def invalidate_cache(self, cache: Cache):
-        # Check for each location in the cache if it is within the current radius
-        if self.radius > 0:
-            cache.remove_locations_outside_radius(self.lat, self.lon, self.radius)
-
-
     def LocationSession(self, request_iterator: Iterator[QueryUpdate], context):
         # could be a sorted set of sorts
-        cache = Cache() 
+        state = SessionState() 
         for query_update in request_iterator:
             if query_update.HasField('pan'):
-                yield from self.handle_pan(query_update.pan)
+                state.lat = query_update.pan.lat
+                state.lon = query_update.pan.lon
             elif query_update.HasField('zoom'):
-                yield from self.handle_zoom(query_update.zoom)
+                state.radius = query_update.zoom.radius
             elif query_update.HasField('parameter'):
-                yield from self.handle_param(query_update.parameter)
+                ref = state.categories if query_update.parameter.type == Parameter.type.CATEGORY else state.attributes
+                if query_update.parameter.add:
+                    ref.add(query_update.parameter.value)
+                else:
+                    ref.remove(query_update.parameter.value)
 
-            self.invalidate_cache(cache)
+            state.invalidate_cache()
+            yield from state.locations
+            yield from self.search_yelp(state)
 
-    def handle_pan(self, pan: LocationPan):
-        # todo
-        self.lat = pan.lat
-        self.lon = pan.lon
-        pass
+    @staticmethod
+    def search_yelp(state: SessionState):
+        """Searches yelp for locations based on the current state of the session. Returns a generator of locations."""
 
-    def handle_zoom(self, zoom: LocationZoom):
-        # todo
-        self.radius = zoom.radius
-        pass
+        builder = YelpHandler.QueryBuilder()
+        builder.latitude(state.lat)
+        builder.longitude(state.lon)
+        builder.radius(state.radius)
+        builder.categories(state.categories)
+        builder.attributes(state.attributes)
+        params = builder.build()
 
-    def handle_param(self, param: Parameter):
-        # todo
-        if param.type == param.type.CATEGORY:
-            self.categories[param.value] = param.add
-        elif param.type == param.type.ATTRIBUTES:
-            self.attributes[param.value] = param.add
-            
-        pass
+        businesses = YelpHandler.search(params)
+        for business in businesses:
+            location = Location.objects.get_or_create(
+                name=business.name,
+                lat=business.latitude,
+                lon=business.longitude,
+                yelp_id=business.id
+            )
+            state.add_location(location)
+            yield location
 
 
 def serve():
