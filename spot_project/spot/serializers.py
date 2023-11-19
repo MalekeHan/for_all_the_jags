8c805ef4c20ceb2dbe91a3071c4cd618477b2270
MalekeHan <Malekehanson@gmail.com>
2023-11-18 16:59:08 -0800

    Serializers lol

--- spot_project/spot/serializers.py
+++ spot_project/spot/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-
+from django_grpc_framework import proto_serializers
+import spot.proto.spot_pb2 as spot_pb2
 """
 from models import MODEL
 """
@@ -14,3 +15,53 @@ class SurveySerializer(serializers.ModelSerializer):
     class Meta:
         model = Survey
         fields = '__all__'  # Or list specific fields if needed
+
+class LocationPanProtoSerializer(proto_serializers.ProtoSerializer):
+    lat = serializers.FloatField()
+    lon = serializers.FloatField()
+
+    class Meta:
+        proto_class = spot_pb2.LocationPan
+
+class LocationZoomProtoSerializer(proto_serializers.ProtoSerializer):
+    radius = serializers.FloatField()
+
+    class Meta:
+        proto_class = spot_pb2.LocationZoom
+
+class ParameterProtoSerializer(proto_serializers.ProtoSerializer):
+    type = serializers.ChoiceField(choices=spot_pb2.FilterType.values)
+    value = serializers.CharField(max_length=100)
+    add = serializers.BooleanField()
+
+    class Meta:
+        proto_class = spot_pb2.Parameter
+
+class LocationProtoSerializer(proto_serializers.ProtoSerializer):
+    id = serializers.CharField(max_length=100)
+    lat = serializers.FloatField()
+    lon = serializers.FloatField()
+    name = serializers.CharField(max_length=100)
+    category = serializers.CharField(max_length=100)
+    attributes = serializers.CharField(max_length=100)
+
+    class Meta:
+        proto_class = spot_pb2.Location
+
+class QueryUpdateProtoSerializer(proto_serializers.ProtoSerializer):
+    pan = LocationPanProtoSerializer()
+    zoom = LocationZoomProtoSerializer()
+    parameter = ParameterProtoSerializer()
+
+    class Meta:
+        proto_class = spot_pb2.QueryUpdate
+
+    def create(self, validated_data):
+        if 'pan' in validated_data: # we need to worry about the one of so conditionally check here
+            return spot_pb2.QueryUpdate(pan=validated_data['pan'])
+        elif 'zoom' in validated_data:
+            return spot_pb2.QueryUpdate(zoom=validated_data['zoom'])
+        elif 'parameter' in validated_data:
+            return spot_pb2.QueryUpdate(parameter=validated_data['parameter'])
+        else:
+            raise serializers.ValidationError("Invalid data")
