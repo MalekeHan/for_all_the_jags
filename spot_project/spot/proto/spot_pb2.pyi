b2bfe7abcb79d1dae3515bf0af9232b63375047a
colmpat <cplang@dons.usfca.edu>
2023-11-18 18:45:35 -0800

    chore: bootstrap grpc location service

--- spot_project/spot/proto/spot_pb2.pyi
+++ spot_project/spot/proto/spot_pb2.pyi
@@ -46,6 +46,14 @@ class Parameter(_message.Message):
     add: bool
     def __init__(self, type: _Optional[_Union[FilterType, str]] = ..., value: _Optional[str] = ..., add: bool = ...) -> None: ...
 
+class StreamUpdate(_message.Message):
+    __slots__ = ["location", "flush"]
+    LOCATION_FIELD_NUMBER: _ClassVar[int]
+    FLUSH_FIELD_NUMBER: _ClassVar[int]
+    location: Location
+    flush: Flush
+    def __init__(self, location: _Optional[_Union[Location, _Mapping]] = ..., flush: _Optional[_Union[Flush, _Mapping]] = ...) -> None: ...
+
 class Location(_message.Message):
     __slots__ = ["id", "lat", "lon", "name", "category", "attributes"]
     ID_FIELD_NUMBER: _ClassVar[int]
@@ -61,3 +69,7 @@ class Location(_message.Message):
     category: str
     attributes: str
     def __init__(self, id: _Optional[str] = ..., lat: _Optional[float] = ..., lon: _Optional[float] = ..., name: _Optional[str] = ..., category: _Optional[str] = ..., attributes: _Optional[str] = ...) -> None: ...
+
+class Flush(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
