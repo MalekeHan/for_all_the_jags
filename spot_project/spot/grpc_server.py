b4e930647982a97a8a7948d6e8b53d53728e4cc6
MalekeHan <Malekehanson@gmail.com>
2023-11-18 19:20:11 -0800

    Did handlers

--- spot_project/spot/grpc_server.py
+++ spot_project/spot/grpc_server.py
@@ -5,6 +5,7 @@ from concurrent import futures
 from spot.proto.spot_pb2 import LocationPan, LocationZoom, Parameter, QueryUpdate
 from spot.proto.spot_pb2_grpc import LocationServiceServicer, add_LocationServiceServicer_to_server
 
+
 class LocationServicer(LocationServiceServicer):
     lat = 0
     lon = 0
@@ -12,10 +13,15 @@ class LocationServicer(LocationServiceServicer):
     categories = {}
     attributes = {}
 
-    # could be a sorted set of sorts
-    cache = {}
+
+    def invalidate_cache(self):
+        pass
+        #check for each member in the cache if it is within the current radius, if not remove it
+
 
     def LocationSession(self, request_iterator: Iterator[QueryUpdate], context):
+        # could be a sorted set of sorts
+        cache = {} # max size 20
         for query_update in request_iterator:
             if query_update.HasField('pan'):
                 yield from self.handle_pan(query_update.pan)
@@ -24,16 +30,26 @@ class LocationServicer(LocationServiceServicer):
             elif query_update.HasField('parameter'):
                 yield from self.handle_param(query_update.parameter)
 
+            self.invalidate_cache
+
     def handle_pan(self, pan: LocationPan):
         # todo
+        self.lat = pan.lat
+        self.lon = pan.lon
         pass
 
     def handle_zoom(self, zoom: LocationZoom):
         # todo
+        self.radius = zoom.radius
         pass
 
     def handle_param(self, param: Parameter):
         # todo
+        if param.type == param.type.CATEGORY:
+            self.categories[param.value] = param.add
+        elif param.type == param.type.ATTRIBUTES:
+            self.attributes[param.value] = param.add
+            
         pass
 
 
